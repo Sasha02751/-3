f9c193669bd843d62dc44fd9a207faa7fdc0d261
Sasha02751 <sasha.kozlov.27062004@gmail.com>
2025-01-16 00:12:42 +0300

    Update Поиск общих участников.py

--- Лабораторная работа 3/Поиск общих участников.py
+++ Лабораторная работа 3/Поиск общих участников.py
@@ -1,19 +1,16 @@
 # TODO Напишите функцию find_common_participants
+def find_common_participants(group1, group2, sep=','):
+    participants1 = set(group1.split(sep))
+    participants2 = set(group2.split(sep))
 
-def find_common_participants (first_group, second_group, separator = ","):
-    g1 = first_group.split(separator)
-    g2 = second_group.split(separator)
-    a = []
-    for i in range(len(g1)):
-        for j in range(len(g2)):
-            if g1[i] == g2[j]:
-                a.append(g1[i])
-    return a
+    common_participants = participants1.intersection(participants2)
+
+    return sorted(common_participants)
+
 
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
 
-result = find_common_participants (participants_first_group, participants_second_group, "|")
-
-print(result)
 # TODO Провеьте работу функции с разделителем отличным от запятой
+result = find_common_participants(participants_first_group, participants_second_group, '|')
+print(result)
